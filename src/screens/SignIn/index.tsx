4dd86dee6b832a5ede53df6962516695dedd116e
Lucas <lucas.hgs03@gmail.com>
2022-04-13 02:29:55 -0300

    Finished SignIn layout

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import brandImg from '@assets/brand.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import { Container } from './styles';
+import { 
+	Container, 
+	Content, 
+	Title, 
+	Brand, 
+	ForgotPasswordButton, 
+	ForgotPasswordLabel 
+} from './styles';
 
 export function SignIn(){
 	return(
-			<Container>
+		<Container>
+			<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+				<Content>
+					<Brand source={brandImg} />
+
+					<Title>Login</Title>
+
 					<Input 
 						placeholder="E-mail"
 						type="secondary"
@@ -21,10 +37,16 @@ export function SignIn(){
 						secureTextEntry
 					/>
 
+					<ForgotPasswordButton>
+						<ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+					</ForgotPasswordButton>
+
 					<Button 
 						title='Entrar'
 						type='secondary'
 					/>
-			</Container>
+				</Content>
+			</KeyboardAvoidingView>
+		</Container>
 	)
 }

8cc74204d58eac690426644c22f2a44cb576a233
Lucas <lucas.hgs03@gmail.com>
2022-04-17 22:06:51 -0300

    feat: Header created

---
+++ src/screens/Product/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Platform, TouchableOpacity } from 'react-native';
+
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel
+} from './styles'; 
+
+export function Product(){
+  return(
+    <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>eletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  )
+}
